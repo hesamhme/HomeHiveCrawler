32a8e456fc4a54a4ab9b21a85eaba8c8285e97d7
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-11-16 18:50:09 +0330

    HSM(feat): read from json and save it in database

--- CrawlerProject/main.go
+++ CrawlerProject/main.go
@@ -1,40 +1,49 @@
 package main
 
 import (
-	"CrawlerProject/internal/bot"
-	"CrawlerProject/internal/repository"
-	"CrawlerProject/pkg/config"
-	"CrawlerProject/pkg/logger"
-	p "CrawlerProject/pkg/postgres"
-	"log"
-	"os"
+    "CrawlerProject/internal/bot"
+    "CrawlerProject/internal/repository"
+    "CrawlerProject/internal/service"
+    "CrawlerProject/pkg/config"
+    "CrawlerProject/pkg/logger"
+    p "CrawlerProject/pkg/postgres"
+    "log"
+    "os"
 )
 
 func main() {
-	config, err := config.InitConfig()
-	if err != nil {
-		logger.Logger.Error().Err(err).Msg("error while initializing config")
-		os.Exit(3)
-	}
-	db := p.NewDBConnection(config.DBHost, config.DBPort, config.DBName, config.DBUser, config.DBPassword)
-	localDB, err := db.InitConnection()
-	if err != nil {
-		logger.Logger.Error().Err(err).Msg("error while initalise database connection")
-		os.Exit(3)
-	}
-	d := repository.NewDatabase(localDB)
-	if err := d.Migrate(); err != nil {
-		logger.Logger.Error().Err(err).Msg("error while migrating tables")
-	}
+    config, err := config.InitConfig()
+    if err != nil {
+        logger.Logger.Error().Err(err).Msg("error while initializing config")
+        os.Exit(3)
+    }
+    db := p.NewDBConnection(config.DBHost, config.DBPort, config.DBName, config.DBUser, config.DBPassword)
+    localDB, err := db.InitConnection()
+    if err != nil {
+        logger.Logger.Error().Err(err).Msg("error while initalise database connection")
+        os.Exit(3)
+    }
+    d := repository.NewDatabase(localDB)
+    if err := d.Migrate(); err != nil {
+        logger.Logger.Error().Err(err).Msg("error while migrating tables")
+    }
 
-	// repositories
+    // repositories
 
-	log.Println("Database tables created/migrated successfully!")
+    service.ReadFromJson(localDB)
 
-	// telegram bot
+    log.Println("Database tables created/migrated successfully!")
 
-	err = bot.SetupBot(config.TGToken)
-	if err != nil {
-		return
-	}
+    // bot
+    bot.SetDB(localDB)
+
+    err = bot.SetupBot(config.TGToken)
+    if err != nil {
+        return
+    }
+
 }
+
+
+
+
