c9ae39f51cc8a32d3a41ce60b85767e6f1326e02
Hesam Hashemi <109273285+hesamhme@users.noreply.github.com>
2024-11-16 18:50:39 +0330

    HSM(update): update lisitng model

--- CrawlerProject/internal/model/listing.go
+++ CrawlerProject/internal/model/listing.go
@@ -6,21 +6,24 @@ import (
 
 type Listing struct {
 	ListingID    uint   `gorm:"primaryKey"`
-	Title        string `gorm:"size:255;not null"`
-	Description  string `gorm:"type:text"`
-	Price        float64
-	City         string `gorm:"size:100"`
-	Neighborhood string `gorm:"size:100"`
-	Area         float64
-	Rooms        int
-	Status       string `gorm:"size:20"` // e.g., "rent", "sell"
-	Floor        *string
-	HasStorage   bool
-	HasElevator  bool
-	Source       string     `gorm:"size:100"` // e.g., "divar", "sheypoor"
-	URL          string     `gorm:"type:text"`
-	ExpiresAt    *time.Time `gorm:"null"` // Expiration date of the listing
-	Images       []string   `gorm:"-"`    // Placeholder for associated images
+	Title        string  `gorm:"size:2048;not null"`
+	Price        float64 `gorm:"not null"`
+	Location     string  `gorm:"size:512"`
+	Description  string  `gorm:"type:text"`
+	Link         string  `gorm:"size:1048;unique;not null"`
+	Seller       string  `gorm:"size:100"`
+	City         string  `gorm:"size:100"`
+	Neighborhood string  `gorm:"size:100"`
+	Meterage     int     `gorm:"not null"`
+	Bedrooms     int     `gorm:"not null"`
+	AdType       string  `gorm:"size:50"` // e.g., "فروش", "اجاره"
+	Age          string  `gorm:"size:50"` // Age as string to capture different formats if needed
+	HouseType    string  `gorm:"size:50"`
+	Floor        int     `gorm:"not null"`
+	Warehouse    bool    `gorm:"not null"`
+	Elevator     bool    `gorm:"not null"`
+	AdCreateDate string  `gorm:"size:50"` // Keeping as string as per your data example
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
+	Images       []string   `gorm:"-"`    // Placeholder for associated images
 }
